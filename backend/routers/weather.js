9fb7bd291095ebed6798e09f477a8ebd7248045e
Azizi Aziz <azizi@dsm.com.my>
2021-06-18 18:15:52 +0800

    Init weather route

---
+++ backend/routers/weather.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoCon from '../modules/mongo.js';
+
+const weatherColl = new mongoCon('cinteractive', 'weather');
+
+const app = express.Router();
+
+app.get('/:city', getWeatherByCity);
+async function getWeatherByCity(req, res) {
+  let mongo = await weatherColl.getCollection();
+  res.status(200).send(await mongo.find({ city: { $regex: `^${req.params.city}`, $options: 'i' } }).toArray());
+}
+
+app.get('/', addDefaultWeather);
+async function addDefaultWeather(req, res) {
+  let mongo = await weatherColl.getCollection();
+  let items = [
+    { city: 'Klang', weather: 'hot' },
+    { city: 'Shah Alam', weather: 'cloudy' }
+  ];
+
+  let insert = await mongo.insertMany(items);
+  res.status(200).send(`Insert ${insert.insertedCount} rows`);
+}
+
+export default app;

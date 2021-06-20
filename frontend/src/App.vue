b74ab626551d74ac0e975f1659f69c5705d0ecd3
Muhammad Azizi <azizi@hotmail.com.my>
2021-06-21 01:58:59 +0800

    Complete the weather popup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,6 +10,40 @@
 				</tr>
 			</thead>
 		</table>
+
+		<div v-if="selectedWeather" class="weather-popup">
+			<div class="weather-content">
+				<div class="title">Weather for<br />{{ selectedCity['city'] }}, {{ selectedCity['state'] }}, {{ selectedCity['country'] }}</div>
+				<div>({{ selectedCity['latlong'] }})</div><br>
+				<div class="title">Forecasted at<br/>{{ selectedWeather['datetime'] }}</div><br>
+				<div class="temperature">
+					<span class="value">{{ (selectedWeather['temp'][isMetric ? 'c' : 'f']).toFixed(2) }}</span>
+					<span>{{ isMetric ? '℃' : '℉' }}</span>
+				</div>
+				<div class="description">
+					<div>{{ selectedWeather['description'] }}</div>
+					<img :src="selectedWeather['icon']" alt="" height="30" width="30">
+				</div><br>
+				<div>
+					<div class="label">Feels Like</div>
+					<span class="value">{{ (selectedWeather['feelslike'][isMetric ? 'c' : 'f']).toFixed(2) }}</span>
+					<span>{{ isMetric ? '℃' : '℉' }}</span>
+				</div><br>
+				<div>
+					<div class="label">Visibility</div>
+					<span>{{ (selectedWeather['visibility'][isMetric ? 'km' : 'mp']).toFixed(2) }} {{ isMetric ? 'km' : 'mi' }}</span>
+				</div><br>
+				<div>
+					<div class="label">Wind Speed</div>
+					<span>{{ (selectedWeather['windspeed'][isMetric ? 'kmh' : 'mph']).toFixed(2) }} {{ isMetric ? 'kmh' : 'mph' }}</span>
+				</div><br>
+
+				<div class="close-popup" @click="selectedWeather = null">X</div>
+				<div class="units">
+					Unit : <span @click="isMetric = !isMetric">{{ isMetric ? 'Metric' : 'Imperial' }}</span>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -19,15 +53,18 @@ export default {
 	components: {},
 	data: function () {
 		return {
+			table: null,
+			selectedCity: null,
+			selectedWeather: null,
+			isMetric: true
 		};
+	},
+	computed: {
 	},
 	props: {},
 	methods: {},
 	mounted() {
-		let table = $("#cityTable").DataTable({
-      // data: this.testData,
-      // ajax: 'http://localhost:3000/cities?page=100',
-      // dataSrc: 'result',
+		this.table = $("#cityTable").DataTable({
       ajax: {
         url: 'http://localhost:3000/cities',
         dataSrc: 'result'
@@ -46,9 +83,15 @@ export default {
 			bLengthChange: false
 		});
 
-		$('#cityTable tbody').on('click', 'tr', function(e) {
-			let data = table.row(this).data();
-			console.log(data);
+		$('#cityTable tbody').on('click', 'tr', async (e) => {
+			let row = e.target.parentNode;
+			this.selectedCity = this.table.row(row).data();
+			console.log(this.selectedCity);
+			this.selectedWeather = (await this.$axios.post('/weather', {
+				time: new Date().getTime(),
+				latlng: this.selectedCity['latlong']
+			})).data;
+			console.log(this.selectedWeather);
 		});
 	},
 	watch: {},
@@ -57,8 +100,90 @@ export default {
 
 <style lang="scss" scoped>
 .app-main {
-  .city-table {
+  > .city-table {
     border: 1px solid black;
   }
+
+	> .weather-popup {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(128,128,128,0.5);
+		z-index: 10;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		> .weather-content {
+			background-color: white;
+			border-radius: 10px;
+			width: 90%;
+			height: 75%;
+			max-width: 500px;
+			padding: 10px;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			text-align: center;
+			position: relative;
+			overflow: auto;
+
+			> .title {
+				font-size: 1.5em;
+			}
+
+			> .temperature {
+				display: flex;
+				align-items: flex-start;
+
+				> .value {
+					font-size: 3em;
+				}
+			}
+
+			> .description {
+				display: flex;
+				align-items: center;
+
+				> img {
+					background-color: rgba(128,128,128,0.3);
+				}
+
+				> * {
+					margin: 5px;
+				}
+			}
+
+			> div > .label {
+				font-weight: bold;
+				font-size: 1.2em;
+				margin-bottom: 5px;
+			}
+
+			> .close-popup {
+				position: absolute;
+				right: 10px;
+				top: 10px;
+				background-color: red;
+				color: white;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				height: 30px;
+				width: 30px;
+				border-radius: 100%;
+				cursor: pointer;
+			}
+
+			> .units {
+				position: absolute;
+				top: 10px;
+				left: 10px;
+				cursor: pointer;
+			}
+		}
+	}
 }
 </style>

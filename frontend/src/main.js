0d995794f7bf909935766e4a4e0ab7fe7f486b8a
Muhammad Azizi <azizi@hotmail.com.my>
2021-06-21 01:59:13 +0800

    Using axios as global with baseurl set to localhost

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Axios from 'axios'
 
 Vue.config.productionTip = false
+
+const axios = Axios
+axios.defaults.baseURL = 'http://localhost:3000'
+Vue.prototype.$axios = axios
 
 new Vue({
   render: h => h(App),

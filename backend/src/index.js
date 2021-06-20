8ddb1011caa91eb4a4b1d56253fe630213ae3d0d
Muhammad Azizi <azizi@hotmail.com.my>
2021-06-21 03:41:19 +0800

    Added env port for heroku

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ import cities from '../routers/cities.js';
 import weather from '../routers/weather.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors({
   origin: 'http://localhost:8080'
